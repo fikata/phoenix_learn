bbb3d12312d5f91074d4e2bacba60841d12a9a73
filip.ninkov <filip.ninkov@sumup.com>
2019-06-28 10:49:02 +0300

    config files for heroku v2

--- apps/rsvp_web/config/prod.exs
+++ apps/rsvp_web/config/prod.exs
@@ -10,8 +10,9 @@ use Mix.Config
 # which you should run after static files are built and
 # before starting your production server.
 config :rsvp_web, RsvpWebWeb.Endpoint,
-  url: [host: "example.com", port: 80],
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  url: [host: "desolate-sea-52952.herokuapp.com", port: 80],
+  cache_static_manifest: "priv/static/manifest.json",
+  secret_key_base: System.get_env("SECRET_KEY_BASE")
 
 # Do not print debug messages in production
 config :logger, level: :info
@@ -62,4 +63,4 @@ config :logger, level: :info
 
 # Finally import the config/prod.secret.exs which loads secrets
 # and configuration from environment variables.
-import_config "prod.secret.exs"
+#import_config "prod.secret.exs"
